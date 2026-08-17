e3da9c1940c0ecdaabbb3acff9e75319e123b0e6
Trannganne <trngan0708@gmail.com>
2026-08-17 10:51:30 +0700

    Thêm đường dẫn cho home page vào app

--- lib/file_picker/filerouter/uploadrouter.dart
+++ lib/file_picker/filerouter/uploadrouter.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterpractisetasks/file_picker/screens/mainscreens/homepage.dart';
 import 'package:flutterpractisetasks/file_picker/screens/mainscreens/uploadpage.dart';
 import 'package:flutterpractisetasks/hub/app_routes/app_routes.dart';
-import 'package:flutterpractisetasks/image_caching/hard/screens/placeholder/placeholder.dart';
 import 'package:flutterpractisetasks/widgets/mainwrapper/customainwrapper.dart';
 import 'package:go_router/go_router.dart';
 
@@ -26,7 +26,7 @@ class Uploadrouter {
               path: AppRoutes.homeUpload,
               builder: (context, state) {
                 debugPrint('Home upload Route');
-                return const PlaceholderTab(label: 'Home upload');
+                return const HomePage();
               },
             ),
           ],
