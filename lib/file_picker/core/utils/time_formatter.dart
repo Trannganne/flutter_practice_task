e5531fe4b45636579235b13b4feecefa4979fc68
Trannganne <trngan0708@gmail.com>
2026-08-21 11:21:48 +0700

    add: Thêm hàm xử lý chuyển đổi thời gian sang chuỗi

--- lib/file_picker/core/utils/time_formatter.dart
+++ lib/file_picker/core/utils/time_formatter.dart
@@ -21,4 +21,11 @@ class TimeFormatter {
     final years = days ~/ 365;
     return '${years}y ago';
   }
+
+  String formatDuration(Duration duration) {
+    final minutes = duration.inMinutes;
+    final seconds = duration.inSeconds % 60;
+
+    return '$minutes:${seconds.toString().padLeft(2, '0')}';
+  }
 }
