e741a962fc804244ec76af8a0a5b4151faca52af
Trannganne <trngan0708@gmail.com>
2026-09-09 17:11:12 +0700

    fix: Chỉnh sửa từ appbar sang sliverappbar và bổ sung thêm chức năng copy đường dẫn ảnh

--- lib/file_picker/screens/mainscreens/uploadpage.dart
+++ lib/file_picker/screens/mainscreens/uploadpage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterpractisetasks/file_picker/bloc/file_bloc.dart';
 import 'package:flutterpractisetasks/file_picker/bloc/file_event.dart';
 import 'package:flutterpractisetasks/file_picker/bloc/file_state.dart';
 import 'package:flutterpractisetasks/file_picker/core/utils/file_size_formatter.dart';
-import 'package:flutterpractisetasks/image_caching/core/customappbar.dart';
 import 'package:flutterpractisetasks/permissions/hard/core/widgets/dialog/dialog.dart';
 import 'package:flutterpractisetasks/permissions/hard/core/widgets/photo_section.dart';
 import 'package:flutterpractisetasks/widgets/common_tabbar/custom_tabbar.dart';
@@ -50,6 +50,32 @@ class _UploadpageState extends State<Uploadpage>
     super.dispose();
   }
 
+  Future<void> _copyUrl(String? url) async {
+    debugPrint('[Copy] URL nhận được: $url');
+
+    if (url == null || url.trim().isEmpty) {
+      if (!mounted) return;
+
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          const SnackBar(content: Text('Không tìm thấy đường dẫn để sao chép')),
+        );
+
+      return;
+    }
+
+    await Clipboard.setData(ClipboardData(text: url));
+
+    if (!mounted) return;
+
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(const SnackBar(content: Text('Đã sao chép đường dẫn')));
+
+    debugPrint('[Copy] Đã sao chép: $url');
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<FileBloc, FileState>(
@@ -60,146 +86,182 @@ class _UploadpageState extends State<Uploadpage>
       },
       builder: (context, state) {
         return Scaffold(
-          appBar: CustomAppbar(
-            title: 'Cloud Media Uploader',
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.notifications_outlined),
-              ),
-            ],
-          ),
-          body: Column(
-            children: [
-              CustomTabBar(
-                tabController: _tabController,
-                labels: ['Upload', 'Gallery'],
-                //icons: [Icons.cloud_upload, Icons.album],
-              ),
-              const SizedBox(height: 8),
-
-              PhotoSection(
-                title: 'Add Media',
-                colorText: Colors.black,
-                photoUrls: state.files.map((f) => f.filePath).toList(),
-                photoCount: 0,
-                onTakePhotoPressed: () {
-                  context.read<FileBloc>().add(
-                    ToggleAddMoreEvent(ImageSource.gallery),
-                  );
-                },
-                onRemovePhotoPressed: (path) {
-                  context.read<FileBloc>().add(RemoveFileEvent(path));
-                },
-                icon: Icons.add,
-                content: 'Add More',
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 1,
-                width: double.infinity,
-                color: Colors.grey[200],
-              ),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 8),
-                    child: Commontext(
-                      title: 'Queue(${state.files.length})',
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Spacer(),
-                  TextButton(
+          // appBar: CustomAppbar(
+          //   title: 'Cloud Media Uploader',
+          //   actions: [
+          //     IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          //     IconButton(
+          //       onPressed: () {},
+          //       icon: Icon(Icons.notifications_outlined),
+          //     ),
+          //   ],
+          // ),
+          body: CustomScrollView(
+            controller: _completedScrollController,
+            slivers: [
+              SliverAppBar(
+                title: Commontext(
+                  title: 'Cloud Media Uploader',
+                  colorText: Colors.white,
+                  fontSize: '18',
+                  fontWeight: FontWeight.bold,
+                ),
+                backgroundColor: Colors.black,
+                pinned: false,
+                floating: true,
+                iconTheme: const IconThemeData(color: Colors.white),
+                actions: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                  IconButton(
                     onPressed: () {},
-                    child: Commontext(
-                      title: 'Clear Completed',
-                      colorText: Colors.blueAccent,
-                    ),
+                    icon: Icon(Icons.notifications_outlined),
                   ),
                 ],
               ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.black,
+                      child: CustomTabBar(
+                        tabController: _tabController,
+                        labels: ['Upload', 'Gallery'],
+                        //icons: [Icons.cloud_upload, Icons.album],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
 
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.files.length,
-                  itemBuilder: (context, index) {
-                    final task = state.files[index];
-                    return FileCard(
-                      taskId: task.id,
-                      title: task.filePath.split('/').last,
-                      status: task.status.name,
-                      width: task.width ?? 0,
-                      height: task.height ?? 0,
-                      fileSize: FileSizeFormatter.format(task.sizeByte ?? 0),
-                      progress: task.progress,
-                      imagePath: task.filePath,
-                      onPausePressed: () {
-                        debugPrint(
-                          '[Uploadpage] Gửi PauseUploadEvent: ${task.id}',
-                        );
-                        ;
-                        context.read<FileBloc>().add(PauseUploadEvent(task.id));
-                      },
-                      onResumePressed: () {
+                    PhotoSection(
+                      title: 'Add Media',
+                      colorText: Colors.black,
+                      photoUrls: state.files.map((f) => f.filePath).toList(),
+                      photoCount: 0,
+                      onTakePhotoPressed: () {
                         context.read<FileBloc>().add(
-                          ResumeUploadEvent(task.id),
+                          ToggleAddMoreEvent(ImageSource.gallery),
                         );
                       },
-                      onCancelPressed: () async {
-                        final result = await CustomDialog().showCusTomDialog(
-                          context,
-                          title: 'Xác nhận',
-                          content: 'Bạn có chắc chắn muốn hủy upload file này?',
-                          button1: 'Tiếp tục',
-                          button2: 'Xác nhận hủy',
-                        );
-
-                        if (result == DialogResult.confirm) {
-                          context.read<FileBloc>().add(
-                            CancelUploadEvent(task.id),
-                          );
-                        } else if (result == DialogResult.cancel) {
-                          context.pop(context);
-                        }
+                      onRemovePhotoPressed: (path) {
+                        context.read<FileBloc>().add(RemoveFileEvent(path));
                       },
-                    );
-                  },
-                ),
-              ),
-
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 8),
-                    child: Commontext(
-                      title: 'Completed(${state.completedFiles.length})',
-                      fontWeight: FontWeight.bold,
+                      icon: Icons.add,
+                      content: 'Add More',
                     ),
-                  ),
-                ],
-              ),
-              // Completed section
-              Expanded(
-                child: ListView.builder(
-                  controller: _completedScrollController,
-                  itemCount: state.completedFiles.length,
-                  itemBuilder: (context, index) {
-                    final task = state.completedFiles[index];
+                    const SizedBox(height: 16),
+                    Container(
+                      height: 1,
+                      width: double.infinity,
+                      color: Colors.grey[200],
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 8),
+                          child: Commontext(
+                            title: 'Queue(${state.files.length})',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Commontext(
+                            title: 'Clear Completed',
+                            colorText: Colors.blueAccent,
+                          ),
+                        ),
+                      ],
+                    ),
 
-                    return Completedcard(
-                      title: task.filePath.split('/').last,
-                      completedAt:
-                          (DateTime.now().millisecondsSinceEpoch -
-                              task.updatedAt) ~/
-                          1000,
-                      sourcePath: task.remoteUrl,
-                      imagePath: task.filePath,
-                    );
-                  },
+                    // Completed section
+                  ],
                 ),
+              ),
+
+              SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                  final task = state.files[index];
+                  return FileCard(
+                    taskId: task.id,
+                    title: task.filePath.split('/').last,
+                    status: task.status.name,
+                    width: task.width ?? 0,
+                    height: task.height ?? 0,
+                    fileSize: FileSizeFormatter.format(task.sizeByte ?? 0),
+                    progress: task.progress,
+                    imagePath: task.filePath,
+                    onCopyPressed: () {
+                      debugPrint("[FileCard] Đã nhấn copy taskId: ${task.id}");
+                      _copyUrl(task.remoteUrl);
+                    },
+                    onPausePressed: () {
+                      debugPrint(
+                        '[Uploadpage] Gửi PauseUploadEvent: ${task.id}',
+                      );
+                      ;
+                      context.read<FileBloc>().add(PauseUploadEvent(task.id));
+                    },
+                    onResumePressed: () {
+                      context.read<FileBloc>().add(ResumeUploadEvent(task.id));
+                    },
+                    onCancelPressed: () async {
+                      final result = await CustomDialog().showCusTomDialog(
+                        context,
+                        title: 'Xác nhận',
+                        content: 'Bạn có chắc chắn muốn hủy upload file này?',
+                        button1: 'Tiếp tục',
+                        button2: 'Xác nhận hủy',
+                      );
+
+                      if (result == DialogResult.confirm) {
+                        context.read<FileBloc>().add(
+                          CancelUploadEvent(task.id),
+                        );
+                      } else if (result == DialogResult.cancel) {
+                        context.pop(context);
+                      }
+                    },
+                  );
+                }, childCount: state.files.length),
+              ),
+
+              SliverToBoxAdapter(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Commontext(
+                        title: 'Completed(${state.completedFiles.length})',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Thay ListView.builder bằng SliverList
+              SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                  final task = state.completedFiles[index];
+
+                  return Completedcard(
+                    title: task.filePath.split('/').last,
+                    completedAt:
+                        (DateTime.now().millisecondsSinceEpoch -
+                            task.updatedAt) ~/
+                        1000,
+                    sourcePath: task.remoteUrl,
+                    imagePath: task.filePath,
+                    onCopyPressed: () async {
+                      // Kiểm tra xem đã copy chưa
+                      debugPrint(
+                        "[Completed Card] Đã nhấn copy taskId: ${task.id}",
+                      );
+                      _copyUrl(task.remoteUrl);
+                    },
+                  );
+                }, childCount: state.completedFiles.length),
               ),
             ],
           ),
